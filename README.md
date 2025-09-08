032b92ee0bf460dc105f09c15b1033198d5193aa
pnwmatt <180812017+pnwmatt@users.noreply.github.com>
2025-09-08 15:59:37 -0700

    shuffle things around

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Heading
+# Docs for SQLRsync.com
+
+The help/ directory is compiled using Eleventy into the webapp/app/pages/help directory.
--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,5 @@ import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 export default function (eleventyConfig) {
 	eleventyConfig.addPlugin(syntaxHighlight, {
 	});
-
 };
 
